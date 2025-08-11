789446213002e32e8f9d6eeed8e7c6cee2d061a7
Joaquín Orlandau Rosso <joaquin.orlandau.rosso@gmail.com>
2025-08-11 13:53:34 -0300

    portafolioterminado

--- portfolio-app/vite.config.js
+++ portfolio-app/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portafolio/',
+  base: '/',
   plugins: [react()],
   publicDir: 'public',
   build: {
